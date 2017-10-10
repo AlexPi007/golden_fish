c168a9049acb471f68c049caa189f0d1a5d14a54
AdrianS <saridacheadrian@outlook.com>
2017-10-10 19:58:55 +0300

    Added Tasks (empty)

--- src/golden_fish/golden_fish.ino
+++ src/golden_fish/golden_fish.ino
@@ -1,6 +1,10 @@
 #include <Arduino_FreeRTOS.h>
 
 //Forward task functions declarations
+void TaskDisplayAndButtons(void *pvParameters);
+void TaskSensorsRead(void *pvParameters);
+void TaskMotor(void *pvParameters);
+void TaskLights(void *pvParameters);
 
 
 void setup()
@@ -8,15 +12,63 @@ void setup()
   //Hardware init:
   
 
-  //Tasks:
-  //@TODO test if: create screen display (and buttons read) task, with navigation menu
-  //@TODO create sensors reading tasks and place information in global variables
-  //@TODO create motor(s) moving task
-  //@TODO create lights task
-  //@TODO implement serial task/macro for degub and computer communication
+  //Tasks init:
+  xTaskCreate(TaskDisplayAndButtons, (const portCHAR *)"DisplayAndButtons", 128, NULL, 3, NULL);
+  xTaskCreate(TaskSensorsRead, (const portCHAR *)"SensorRead", 128, NULL, 1, NULL);
+  xTaskCreate(TaskMotor, (const portCHAR *)"Motor", 128, NULL, 3, NULL);
+  xTaskCreate(TaskLights, (const portCHAR *)"Lights", 128, NULL, 1, NULL);
 }
 
 void loop()
 {
 // DO NOT ADD CODE HERE. Things are done ONYL in Tasks.
 }
+
+void TaskDisplayAndButtons(void *pvParameters)
+{
+  (void) pvParameters;
+  //@TODO Add initial task setup
+  
+  for (;;)
+  {
+    //@TODO Add continous functionality
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskSensorsRead(void *pvParameters)
+{
+  (void) pvParameters;
+  //@TODO Add initial task setup
+
+   for (;;)
+  {
+    //@TODO Add continous functionality
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskMotor(void *pvParameters)
+{
+  (void) pvParameters;
+  //@TODO Add initial task setup
+
+   for (;;)
+  {
+    //@TODO Add continous functionality
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskLights(void *pvParameters)
+{
+  (void) pvParameters;
+  //@TODO Add initial task setup
+
+   for (;;)
+  {
+    //@TODO Add continous functionality
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
