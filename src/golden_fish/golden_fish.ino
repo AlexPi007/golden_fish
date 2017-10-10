1d83703c6de221f70c75cd462c03be5aa0c0bf15
AdrianS <saridacheadrian@outlook.com>
2017-10-10 14:48:11 +0300

    ...

--- src/golden_fish/golden_fish.ino
+++ src/golden_fish/golden_fish.ino
@@ -1,18 +1,19 @@
 #include <Arduino_FreeRTOS.h>
 
+//Forward task functions declarations
+
+
 void setup()
 {
-  // put your setup code here, to run once:
+  //Hardware init:
+  
 
-  //TASKS:
+  //Tasks:
   //@TODO test if: create screen display (and buttons read) task, with navigation menu
   //@TODO create sensors reading tasks and place information in global variables
   //@TODO create motor(s) moving task
   //@TODO create lights task
   //@TODO implement serial task/macro for degub and computer communication
-
-  //GENERAL NOTICE:
-  //@TODO implement some mutex to execute tasks in right order and with correct information
 }
 
 void loop()
