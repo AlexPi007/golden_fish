66ab38369bc22e6b66b859bc3c10effdf8f43219
AdrianS <saridacheadrian@outlook.com>
2017-10-10 09:44:04 +0300

    Added @TODOs

--- src/golden_fish/golden_fish.ino
+++ src/golden_fish/golden_fish.ino
@@ -1,11 +1,21 @@
 #include <Arduino_FreeRTOS.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
+  //TASKS:
+  //@TODO test if: create screen display (and buttons read) task, with navigation menu
+  //@TODO create sensors reading tasks and place information in global variables
+  //@TODO create motor(s) moving task
+  //@TODO create lights task
+  //@TODO implement serial task/macro for degub and computer communication
+
+  //GENERAL NOTICE:
+  //@TODO implement some mutex to execute tasks in right order and with correct information
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+// DO NOT ADD CODE HERE. Things are done ONYL in Tasks.
 }
