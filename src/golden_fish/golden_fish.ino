0a4f3689ce9f1d4c2059c3a29bd14971e6a0f7d7
AdrianS <saridacheadrian@outlook.com>
2017-10-12 09:19:23 +0300

    LCD and buttons read

--- src/golden_fish/golden_fish.ino
+++ src/golden_fish/golden_fish.ino
@@ -1,4 +1,17 @@
 #include <Arduino_FreeRTOS.h>
+#include <LiquidCrystal.h>
+//LCD
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+//Defines:
+#define b_Select 0
+#define b_Left 1
+#define b_Right 2
+#define b_Up 3
+#define b_Down 4
+#define b_ERROR 5
+#define TRUE 1
+#define FALASE 0
 
 //Forward task functions declarations
 void TaskDisplayAndButtons(void *pvParameters);
@@ -7,11 +20,15 @@ void TaskFeed(void *pvParameters);
 void TaskLights(void *pvParameters);
 void TaskLWater(void *pvParameters);
 
+//Forward general function declarations
+int ButtonRead();
+
 
 void setup()
 {
   //Hardware init if needed global and comments about task reserved hardware:
-  
+  //LCD reserved pins: 4;5;6;7;8;9
+  //Button read pin: A0
 
   //Tasks init:
   xTaskCreate(TaskDisplayAndButtons, (const portCHAR *)"DisplayAndButtons", 128, NULL, 2, NULL);
@@ -30,11 +47,15 @@ void TaskDisplayAndButtons(void *pvParameters)
 {
   (void) pvParameters;
   //@TODO Add initial task setup
-  
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("TaskDnB set.");
+  uint16_t iterator = 0;
   for (;;)
   {
     //@TODO Add continous functionality
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
     //@TODO read pin state (all buttons are on one pin) and implement DEBOUNCE
     //Find a way to check is message is correctly displayed
     //Implement a menu (via a state machine) with "screens" navigable, has a selectable luminosity and a 'feed now'
@@ -95,3 +116,33 @@ void TaskWater(void *pvParameters)
   }
 }
 
+
+//Functions:
+int ButtonRead()
+{
+  uint8_t readA0;
+  readA0 = analogRead(0);
+  delay(5);
+  if (readA0 < 50)
+  {
+    return b_Right;  
+  }
+  if (readA0 < 195)
+  {
+    return b_Up; 
+  }
+  if (readA0 < 380)
+  {
+    return b_Down; 
+  }
+  if (readA0 < 555)
+  {
+    return b_Left; 
+  }
+  if (readA0 < 790)
+  {
+    return b_Select;   
+  }
+  //If none of the above is true, means an error has occured.
+  return b_ERROR;
+}
